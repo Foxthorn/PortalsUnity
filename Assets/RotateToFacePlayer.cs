29d34272bc63441a788086ca607eef35549bb7ca
Charl Pauwels (Manpower SA (Pty) Ltd) <v-chpauw@microsoft.com>
2018-05-10 10:00:17 +0200

    Add functionality for multiple portals

---
+++ Assets/RotateToFacePlayer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class RotateToFacePlayer : MonoBehaviour
+{
+    public Transform player;
+    public Transform world;
+    public Transform renderPlane;
+    public Renderer renderPlaneGO;
+
+    public Material CameraMatA;
+    public Material CameraMatB;
+
+    private bool playerIsBehind = false;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 portalToPlayer = player.position - transform.position;
+        float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+        if (dotProduct < 0f)
+        {
+            renderPlane.Rotate(Vector3.back, 180);
+            if (renderPlaneGO.material.name == "CameraMat_B (Instance)")
+            {
+                renderPlaneGO.material = CameraMatA;
+            }
+            else if (renderPlaneGO.material.name == "CameraMat_A (Instance)")
+            {
+                renderPlaneGO.material = CameraMatB;
+            }
+        }
+    }
+}
